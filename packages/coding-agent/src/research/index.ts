6669c7abb3c13baa577a998bf6d97f67170b6f20
Dae | Snaiily <snaillydevs@gmail.com>
2026-08-31 00:19:38 +0200

    feat(research): export autonomous research subsystem

---
+++ packages/coding-agent/src/research/index.ts
@@ -0,0 +1,9 @@
+export * from "./types";
+export * from "./policy";
+export * from "./query";
+export * from "./security";
+export * from "./sources";
+export * from "./extract";
+export * from "./cache";
+export * from "./engine";
+export { ResearchTool, researchSchema } from "./tool";
