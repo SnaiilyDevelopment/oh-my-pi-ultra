a5a067c8a335ddfd33cbcdebfbed72a17b14faac
Dae | Snaiily <snaillydevs@gmail.com>
2026-08-30 23:49:03 +0200

    feat(instructions): export instruction architecture

---
+++ packages/coding-agent/src/instructions/index.ts
@@ -0,0 +1,7 @@
+export { baseInstructionLayer } from "./base";
+export { composeInstructions } from "./compose";
+export { phaseInstructionLayer } from "./phase";
+export { recoveryInstructionLayer } from "./recovery";
+export { specialistInstructionLayer } from "./specialist";
+export { taskInstructionLayer, taskKindForProfile } from "./task";
+export type * from "./types";
