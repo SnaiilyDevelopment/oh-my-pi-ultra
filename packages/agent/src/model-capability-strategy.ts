d9b2a2fe9ae26befd50f26f219b9fb11cab4bc08
Dae | Snaiily <snaillydevs@gmail.com>
2026-08-30 21:02:46 +0200

    feat: expose model capability strategy API

---
+++ packages/agent/src/model-capability-strategy.ts
@@ -0,0 +1,2 @@
+export { createModelCapabilityTelemetry, createStrategyProfile, deriveModelCapabilities, getModelCapabilityCacheSize, invalidateModelCapabilities, recordCapabilityEvidence } from "./model-capability";
+export { currentCapabilityProfile, effectiveVerificationDepth, getModelCapabilities, getModelStrategy, shouldUseParallelTools } from "./model-capability-runtime";
